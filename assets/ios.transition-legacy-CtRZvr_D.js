0a96f14aefe62340707a2ae73ab8f08145cfbb69
paaumarket <>
2024-04-28 14:37:06 +0000

    Update from https://github.com/paaumarket/paaumarket-ionic-app/commit/c68e73fdeeb64aafbcf4dbdbc14106c62dd97a9c

--- assets/ios.transition-legacy-CtRZvr_D.js
+++ assets/ios.transition-legacy-CtRZvr_D.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DJov0Jt8.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-D1krYHxQ.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
