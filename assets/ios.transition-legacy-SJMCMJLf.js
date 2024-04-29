f604c5e1cfb2e82b28a4e8ec95a8b3531c642992
paaumarket <>
2024-04-29 13:06:23 +0000

    Update from https://github.com/paaumarket/paaumarket-ionic-app/commit/bd90fe9bd4bac5f3f11230eef091a5f13e264cf2

--- assets/ios.transition-legacy-SJMCMJLf.js
+++ assets/ios.transition-legacy-SJMCMJLf.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-xdml7oad.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-CB24USB2.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
