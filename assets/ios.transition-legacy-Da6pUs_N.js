de936461585c163b5fb433bdcc488664b9f07a83
paaumarket <>
2024-04-30 03:00:20 +0000

    Update from https://github.com/paaumarket/paaumarket-ionic-app/commit/9853341a8f812232c5fb8dcbfb1623e9f8410eb8

--- assets/ios.transition-legacy-Da6pUs_N.js
+++ assets/ios.transition-legacy-Da6pUs_N.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DjnXejMj.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-CZhLqWsN.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
