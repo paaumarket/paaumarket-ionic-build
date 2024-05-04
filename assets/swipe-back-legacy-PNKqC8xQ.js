27d80521486b81202b9720c81953deb63d4b82aa
paaumarket <>
2024-05-04 07:50:36 +0000

    Update from https://github.com/paaumarket/paaumarket-ionic-app/commit/73c34be2cb474b8ee2a033551d435c43d630997b

--- assets/swipe-back-legacy-PNKqC8xQ.js
+++ assets/swipe-back-legacy-PNKqC8xQ.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-WZ6dqaAD.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-CB8uOLcm.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
