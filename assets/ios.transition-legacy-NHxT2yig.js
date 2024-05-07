470991769cee09883153e96e4440a8f8cf5dabfc
paaumarket <>
2024-05-07 20:25:18 +0000

    Update from https://github.com/paaumarket/paaumarket-ionic-app/commit/fde24c6c3ecf79f63bcb53e6ca4e6e9b53fa0a68

--- assets/ios.transition-legacy-NHxT2yig.js
+++ assets/ios.transition-legacy-NHxT2yig.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DyNN4hYQ.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-sjPVsoUA.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/md.transition-legacy-DpMAdGJ9.js
+++ assets/md.transition-legacy-DpMAdGJ9.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DyNN4hYQ.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-sjPVsoUA.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
