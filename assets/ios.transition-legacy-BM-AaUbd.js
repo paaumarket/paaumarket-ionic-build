184c4b1e1387bda63cfdab36a15257797dfb9ec6
paaumarket <>
2024-07-09 12:09:29 +0000

    Update from https://github.com/paaumarket/paaumarket-ionic-app/commit/e548f85ac35a108d0c9f37925d944d4b01278bc4

--- assets/ios.transition-legacy-BM-AaUbd.js
+++ assets/ios.transition-legacy-BM-AaUbd.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BhiD_-c3.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-C_2mWSkr.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/swipe-back-legacy-N4tvotjh.js
+++ assets/swipe-back-legacy-N4tvotjh.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BhiD_-c3.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-C_2mWSkr.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
