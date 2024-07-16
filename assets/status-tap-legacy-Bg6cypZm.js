b3d6a7aa4958286dd4b062e0a2abeb9054d89aee
paaumarket <>
2024-07-16 18:17:26 +0000

    Update from https://github.com/paaumarket/paaumarket-ionic-app/commit/a371ead8de6f92a74985ea51c1d3ec51970d314a

--- assets/status-tap-legacy-Bg6cypZm.js
+++ assets/status-tap-legacy-Bg6cypZm.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-B1AEgIfX.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.b,o=e.w,i=e.s}],execute:function(){
+System.register(["./index-legacy-C9AuE7wB.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.b,o=e.w,i=e.s}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
